2ef8651f518ca967d95fb7a9b30c109f7d761cf1
Thakisis <sub722@gmail.com>
2024-08-13 19:02:46 +0200

    fix video not loading

--- components/Videofull/VideoFull.jsx
+++ components/Videofull/VideoFull.jsx
@@ -15,7 +15,7 @@ const VideoFull = ({ setVideo, videoFile = "underwater", style }) => {
 
         <video className=" top-0 w-full h-screen z-0 absolute object-cover" autoPlay playsInline loop muted width="1920px" height="1080px"  >
             <source src={`/videos/${videoFile}.avif`} type="video/avif" />
-            <source src={`/videos/${videoFile}.m4v`} type="video/mp4" />
+            <source src={`/videos/${videoFile}.mp4`} type="video/mp4" />
         </video>
 
     )
