211657ee1c667b3bc8a3acf55379114e741a5209
Thakisis <sub722@gmail.com>
2024-08-13 20:07:40 +0200

    change useRouter with Link

--- components/navbar/LangSelector/LangLink/LangLink.jsx
+++ components/navbar/LangSelector/LangLink/LangLink.jsx
@@ -1,21 +1,18 @@
-"use client"
-import { useRouter, Link, pathnames } from '@/navigation'
-import { usePathname } from 'next/navigation'
+
+import { Link } from '@/navigation'
+
 
 function LangElement({ locale, lang, langName }) {
     const checkSelected = locale === lang ? <span className="ml-auto">✓ </span> : <span className="ml-auto opacity-0">✓ </span>
-    const pathname = usePathname()
-    const router = useRouter()
-
     return (
-        <div
-            onClick={() => { router.replace(pathname, { locale: lang }) }}
+        <Link href="/" locale={lang}
+
             className={`px-3 py-3 flex  items-center ${lang === locale && "bg-gray-800"} hover:bg-gray-800 no-underline hover:no-underline transition-colors duration-100`}
         >
             <span className="block  h-[.9rem] aspect-[4/3]  mr-3" style={{ backgroundImage: `url(/${lang}.svg)` }}></span>
             <span className="inline-block ml text-white mr-2" >{langName}</span>
             {checkSelected}
-        </div>
+        </Link>
 
     )
 }
